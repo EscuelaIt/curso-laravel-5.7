a92601904d1667193b02ff1be4d59b167cc525a2
JuanDMeGon <juandmegon@gmail.com>
2018-09-24 13:58:31 -0500

    Clase 9: Trabajando un poco con el frontend (Laravel Mix)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,6 +22,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //cambio
         return view('home');
     }
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .copy('node_modules/buffer/bin/update-authors.sh', 'public/sh/update-authors.sh')
+   .version()
+   .browserSync({
+        host: 'miproyecto.sync',
+        open: 'external',
+        proxy: 'miproyecto.test',
+        browser: 'Chrome',
+   });
